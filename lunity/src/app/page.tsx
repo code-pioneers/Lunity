5b373ef12fd3659e64a3a9829a3fc9203dea769e
Bertijn Pauwels <bertijn.pauwels@gmail.com>
2024-04-04 15:13:55 +0200

    Hide nav

--- lunity/src/app/page.tsx
+++ lunity/src/app/page.tsx
@@ -9,7 +9,7 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
+      {/* <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -21,7 +21,7 @@ export default function Home() {
             </Link>
           ))}
         </ul>
-      </nav>
+      </nav> */}
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
