d8bb372d30ba5435effd3792d472d84a7df1c180
Bertijn Pauwels <bertijn.pauwels@gmail.com>
2024-06-23 20:02:17 +0200

    Typescript fix

--- lunity-v2/components/elements/SocialMedia.tsx
+++ lunity-v2/components/elements/SocialMedia.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const SocialMedia = () => {
   return (
     <ul className="social-media ">
-      {SocialIcons?.map((item,index)=>{
+      {SocialIcons?.map((item: any,index)=>{
         return(
           <li className="social-media__item" key={index}>
             <Link href={item?.href} className="social-media__link">{item?.icon}</Link>
